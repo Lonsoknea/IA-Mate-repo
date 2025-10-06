4f3e4df63ec3dfdd23d8bc8376772e1c7d35116b
Lonsoknea <lonsoknea07@gmail.com>
2025-10-06 14:42:37 +0700

    fix: remove assignment in while condition (no-cond-assign) in wrapText

--- frontend/src/IAFlowchart.js
+++ frontend/src/IAFlowchart.js
@@ -1128,7 +1128,8 @@ function IAFlowchart() {
       const y = text.attr('y');
       const dy = 0;
       let tspan = text.text(null).append('tspan').attr('x', 0).attr('y', y).attr('dy', dy + 'em');
-      while (word = words.pop()) {
+      while (words.length) {
+        word = words.pop();
         line.push(word);
         tspan.text(line.join(' '));
         if (tspan.node().getComputedTextLength() > width) {
