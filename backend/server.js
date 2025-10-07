c55e5bde0ce07cf666b0a407458b6c56f0fef157
Lonsoknea <lonsoknea07@gmail.com>
2025-10-07 10:04:14 +0700

    fix deploy backend 2

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,10 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://ia-mate-repo.onrender.com',
+  credentials: true
+}));
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
